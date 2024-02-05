3f008b803ad8d308cfcc87f708d7cfa56a2f4ce7
Slowspeaker <isaev.ramazan@outlook.com>
2024-02-06 05:43:05 +0600

    main appl

---
+++ src/test/java/kz/greetgo/xnkqionsbdarpbwmocho/XnkqionsbdarpbwmochoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.greetgo.xnkqionsbdarpbwmocho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XnkqionsbdarpbwmochoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
